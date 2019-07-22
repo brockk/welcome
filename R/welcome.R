ca8cf880931bba76a59fabc3a2896b1c0c13e7f8
Kristian Brock <kristian.brock@gmail.com>
2019-07-22 10:53:43 +0100

    Created simple project

---
+++ R/welcome.R
@@ -0,0 +1,17 @@
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+
+#' Welcomes the user.
+#'
+#' @return Nothing.
+#' @export
+#'
+#' @examples
+#' welcome()
+welcome <- function() {
+  cat("You look great!\n")
+}
